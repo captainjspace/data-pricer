df860695a0ec248d016baff3126107ed8efb499f
Joshua Landman <joshua.s.landman@gmail.com>
2021-02-11 00:42:44 -0800

    Incremented Version and added Glossary
    * Glossary needs entries
    * added docker run script
    * cleaned up requirements to minimum

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ from formatters import formats
 
 # aoplication fields
 globals = conf_global.get()
-version='v0.0.8'+'{}'.format(time.time())
+version='v0.0.10'+'{}'.format(time.time())
 app = Flask(__name__)
 
 
---
+++ docker_run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --rm  -p 80:80  data-pricer
